3ea2e13f3970d28aeff509295873fe098a7b61df
VicentCode <vicentcode@gmail.com>
2021-03-06 21:58:14 -0600

    3

--- app/src/main/java/com/vicentcode/converte/MainActivity.java
+++ app/src/main/java/com/vicentcode/converte/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     String km = "Kilometros";
     String mtros = "Metros";
     String cm = "Centimetros";
+    String mm = "Milímetro";
+    String in = "Pulgadas";
+    String dm = "Decimetro";
+
     String unidad1, unidad2;
     Double valorINT, resultado;
     String resultados;
@@ -39,10 +42,12 @@ public class MainActivity extends AppCompatActivity {
         mLongiList.add(new Longitud_Item(mtros, R.drawable.metro));
         mLongiList.add(new Longitud_Item(km, R.drawable.km));
         mLongiList.add(new Longitud_Item(cm, R.drawable.km));
+        mLongiList.add(new Longitud_Item(dm, R.drawable.km));
+        mLongiList.add(new Longitud_Item(in, R.drawable.km));
+        mLongiList.add(new Longitud_Item(mm, R.drawable.km));
 
-        Button btnLongi = findViewById(R.id.btnCon);
         EditText conver = findViewById(R.id.edLong);
-        TextView txtResult = findViewById(R.id.showResultaod);
+        EditText conver1 = findViewById(R.id.edLong2);
         Spinner spinnerLongi = findViewById(R.id.spinnerLongi);
         Spinner spinnerLongi2 = findViewById(R.id.spinnerLongi2);
         mAdapter = new LongitudAdapter(this, mLongiList);
@@ -61,7 +66,10 @@ public class MainActivity extends AppCompatActivity {
                     unidad1 = km;
                 }else if (unidadSP1.equals(mtros)) {
                     unidad1 = mtros;
+                }else if (unidadSP1.equals(dm)) {
+                    unidad1 = dm;
                 }
+
 
                /* btnLongi.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -92,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
                     unidad2 = km;
                 }else if (unidadSP2.equals(mtros)) {
                     unidad2 = mtros;
+                }else if (unidadSP2.equals(dm)) {
+                    unidad2 = dm;
                 }
             }
             @Override
@@ -104,43 +114,123 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 valorED = conver.getText().toString();
                 valorINT = Double.valueOf(valorED);
-                converte(valorINT, txtResult);
-                conver.setText("123456");
+                converteED(valorINT, conver1);
+
+                //conver.setText("123456");
+            }
+        });
+        conver1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorED = conver1.getText().toString();
+                valorINT = Double.valueOf(valorED);
+                converteED1(valorINT, conver);
             }
         });
 
     }
 
-    public void converte (Double valor, TextView showTXT) {
+    public void converteED (Double valor, EditText showED) {
 
         if (unidad1.equals(mtros) && unidad2.equals(mtros)) {
             resultado = valor;
-            showTXT.setText(resultados = String.valueOf(resultado) + " m");
+            showED.setText(resultados = String.valueOf(resultado));
 
         } else if (unidad1.equals(mtros) && unidad2.equals(km)) {
             resultado = valor / 1000;
-            showTXT.setText(resultados = String.valueOf(resultado) + " km");
+            showED.setText(resultados = String.valueOf(resultado));
+
+        }else if (unidad1.equals(mtros) && unidad2.equals(dm)) {
+            resultado = valor*10;
+            showED.setText(resultados = String.valueOf(resultado));
 
         }else if (unidad1.equals(mtros) && unidad2.equals(cm)) {
             resultado = valor * 100;
-            showTXT.setText(resultados = String.valueOf(resultado) + " cm");
+            showED.setText(resultados = String.valueOf(resultado));
 
-        }else if (unidad1.equals(km) && unidad2.equals(mtros)) {
+        } else if (unidad1.equals(km) && unidad2.equals(mtros)) {
             resultado = valor * 1000;
-            showTXT.setText(resultados = String.valueOf(resultado) + " m");
+            showED.setText(resultados = String.valueOf(resultado));
 
-        }else if (unidad1.equals(km) && unidad2.equals(cm)) {
+        }else if (unidad1.equals(km) && unidad2.equals(dm)) {
+            resultado = valor * 10000;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        } else if (unidad1.equals(km) && unidad2.equals(cm)) {
             resultado = valor * 100000;
-            showTXT.setText(resultados = String.valueOf(resultado) + " cm");
+            showED.setText(resultados = String.valueOf(resultado));
 
-        }else if (unidad1.equals(cm) && unidad2.equals(mtros)) {
+        } else if (unidad1.equals(cm) && unidad2.equals(mtros)) {
             resultado = valor / 100;
-            showTXT.setText(resultados = String.valueOf(resultado) + " m");
+            showED.setText(resultados = String.valueOf(resultado));
 
-        }else if (unidad1.equals(cm) && unidad2.equals(km)) {
+        } else if (unidad1.equals(cm) && unidad2.equals(km)) {
             resultado = valor / 100000;
-            showTXT.setText(resultados = String.valueOf(resultado) + " km");
+            showED.setText(resultados = String.valueOf(resultado));
+
+        }else if (unidad1.equals(cm) && unidad2.equals(dm)) {
+            resultado = valor / 10;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        } else if (unidad1.equals(dm) && unidad2.equals(mtros)) {
+            resultado = valor / 10;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        } else if (unidad1.equals(dm) && unidad2.equals(km)) {
+            resultado = valor / 10000;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        } else if (unidad1.equals(dm) && unidad2.equals(cm)) {
+            resultado = valor * 10;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        } else if (unidad1.equals(dm) && unidad2.equals(in)) {
+            resultado = valor * 3.937;
+            System.out.println(resultado +"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+            showED.setText(resultados = String.valueOf(resultado));
+            System.out.println(resultados +"wwwwwwwwwwwwwwwwwwwwwwwwwwwwsssssssssssssssswwwwww");
+        }else if (unidad1.equals(dm) && unidad2.equals(mm)) {
+            resultado = valor * 1000;
+            showED.setText(resultados = String.valueOf(resultado));
 
         }
     }
+
+    public void converteED1 (Double valor, EditText showED) {
+
+        if (unidad2.equals(mtros) && unidad1.equals(mtros)) {
+            resultado = valor;
+            showED.setText(resultados = String.valueOf(resultado) );
+
+        } else if (unidad2.equals(mtros) && unidad1.equals(km)) {
+            resultado = valor / 1000;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        }else if (unidad2.equals(mtros) && unidad1.equals(cm)) {
+            resultado = valor * 100;
+            showED.setText(resultados = String.valueOf(resultado) );
+
+        }else if (unidad2.equals(km) && unidad1.equals(mtros)) {
+            resultado = valor * 1000;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        }else if (unidad2.equals(km) && unidad1.equals(cm)) {
+            resultado = valor * 100000;
+            showED.setText(resultados = String.valueOf(resultado) );
+
+        }else if (unidad2.equals(cm) && unidad1.equals(mtros)) {
+            resultado = valor / 100;
+            showED.setText(resultados = String.valueOf(resultado) );
+
+        }else if (unidad2.equals(cm) && unidad1.equals(km)) {
+            resultado = valor / 100000;
+            showED.setText(resultados = String.valueOf(resultado) );
+
+        }else if (unidad2.equals(dm) && unidad1.equals(mtros)) {
+            resultado = valor / 10;
+            showED.setText(resultados = String.valueOf(resultado));
+
+        }
+    }
+
 }
